5dbb10a013f561b6159cd9ecc2121acd6cb67d46
Wirhui <huiwang@lenzetech.com>
2019-08-05 14:32:14 +0800

    添加微信小程序iTAG DEMO

--- WX-XCX-BLE/WXProjects/pages/ble/ble.js
+++ WX-XCX-BLE/WXProjects/pages/ble/ble.js
@@ -26,6 +26,9 @@ Page({
     devices: [],
     connected: false,
     chs: [],
+    writeUUid:"",
+    writeCharacteristic:"",
+    deviceId:""
   },
   /*1.打开蓝牙适配器 判断用户的蓝牙设备是否可用*/
   openBluetoothAdapter() {
@@ -106,10 +109,14 @@ Page({
         /*如果发现设备为目标设备，直接进行连接*/
         /*3.对扫描结果进行过滤，符合条件的设备进行连接*/
         /*这里我们通过设备名进行过滤 如果名字以 iTag进行开头或者tongche开头，进行连接*/
-        if ( device.name.startsWith("tongche"))
+        if (device.name.startsWith("iTAG") || device.name.startsWith("tongche"))
         {
           /*连接到设备*/
-          connectDeviceByDeviceId(device.deviceId);
+          this.stopBluetoothDevicesDiscovery();
+          // connectDeviceByDeviceId(device.deviceId);
+          this.connectDeviceByDeviceId(device.deviceId)
+
+         
         }
 
         // if (device.name.toString.startsWith("iTag") || device.name.toString.startsWith("tongche")) {
@@ -145,7 +152,6 @@ Page({
       success: (res) => {
         this.setData({
           connected: true,
-          name,
           deviceId,
         })
         /**/
@@ -165,20 +171,34 @@ Page({
       canWrite: false,
     })
   },
+  /*4.获取蓝牙的服务和特征*/
   /*获取蓝牙所有的服务Services*/
   getBLEDeviceServices(deviceId) {
     wx.getBLEDeviceServices({
       deviceId,
       success: (res) => {
         for (let i = 0; i < res.services.length; i++) {
-          if (res.services[i].isPrimary) {
+          /*根据协议，用到两个服务*/
+          /*服务和特征的关系：一个蓝牙可以有多个服务，一个服务下可以有多个特征*/
+          /*小程序和蓝牙设备进行通讯，就是在确定的设备id,确定的服务，确定的特征下进行首发消息*/
+          /*响铃 服务1802 特征值：2A06 属性：写 设备向蓝牙设备发送消息需要使用这个服务*/
+          /*通知服务FFE0 特征值：FFE1 属性：通知 设备接受蓝牙设备发送的按键消息需要使用这个服务*/
+          /*其他功能请根据自己需要进行扩展*/
+          if (res.services[i].uuid == "00001802-0000-1000-8000-00805F9B34FB") {
+            console.log("发现服务 响铃服务 1802");
+             /*开始获取特征值*/
             this.getBLEDeviceCharacteristics(deviceId, res.services[i].uuid)
-            return
+          }
+          else if (res.services[i].uuid == "0000FFE0-0000-1000-8000-00805F9B34FB") {
+            console.log("发现服务 设备按键通知服务 FFE0");
+            /*开始获取特征值*/
+            this.getBLEDeviceCharacteristics(deviceId, res.services[i].uuid)
           }
         }
       }
     })
   },
+  /*4.获取蓝牙的服务和特征*/
   /*获取服务下所有的特征值Characteristics*/
   getBLEDeviceCharacteristics(deviceId, serviceId) {
     wx.getBLEDeviceCharacteristics({
@@ -187,31 +207,68 @@ Page({
       success: (res) => {
         console.log('getBLEDeviceCharacteristics success', res.characteristics)
         for (let i = 0; i < res.characteristics.length; i++) {
-          let item = res.characteristics[i]
-          if (item.properties.read) {
-            wx.readBLECharacteristicValue({
-              deviceId,
-              serviceId,
-              characteristicId: item.uuid,
-            })
-          }
-          if (item.properties.write) {
-            this.setData({
-              canWrite: true
-            })
-            this._deviceId = deviceId
-            this._serviceId = serviceId
-            this._characteristicId = item.uuid
-            this.writeBLECharacteristicValue()
-          }
-          if (item.properties.notify || item.properties.indicate) {
+
+          // let item = res.characteristics[i]
+          // if (item.properties.read) {
+          //   wx.readBLECharacteristicValue({
+          //     deviceId,
+          //     serviceId,
+          //     characteristicId: item.uuid,
+          //   })
+          // }
+          // if (item.properties.write) {
+          //   this.setData({
+          //     canWrite: true
+          //   })
+          //   this._deviceId = deviceId
+          //   this._serviceId = serviceId
+          //   this._characteristicId = item.uuid
+          //   this.writeBLECharacteristicValue()
+          // }
+          // if (item.properties.notify || item.properties.indicate) {
+          //   wx.notifyBLECharacteristicValueChange({
+          //     deviceId,
+          //     serviceId,
+          //     characteristicId: item.uuid,
+          //     state: true,
+          //   })
+          // }
+
+
+          /*由于我们已经先前得到蓝牙的协议，可以直接进行判断，对改特征值进行修改*/
+
+          /*4.监听防丢器设备发来的按键消息*/
+          if (res.characteristics[i].uuid =="0000FFE1-0000-1000-8000-00805F9B34FB")
+          {
+            console.log("开始监听设备按键消息");
+
             wx.notifyBLECharacteristicValueChange({
               deviceId,
               serviceId,
-              characteristicId: item.uuid,
+              characteristicId: res.characteristics[i].uuid,
               state: true,
             })
           }
+           /*5.向防丢器发送消息，控制防丢器的响铃与不响铃*/
+           /*记录可以写入的服务*/
+          else if (res.characteristics[i].uuid == "00002A06-0000-1000-8000-00805F9B34FB"){
+            console.log("获取到写入特征");
+            /*把可以写入的服务和特征进行记录*/
+            this.setData(
+              {
+                deviceId:deviceId,
+                writeUUid:serviceId,
+                writeCharacteristic: res.characteristics[i].uuid
+              }
+            );
+            // this._deviceId = deviceId
+            // this._serviceId = serviceId
+            // this._characteristicId = res.characteristics[i].uuid
+            // this.writeBLECharacteristicValue()
+          }
+
+
+
         }
       },
       fail(res) {
@@ -220,6 +277,7 @@ Page({
     })
     // 操作之前先监听，保证第一时间获取数据
     wx.onBLECharacteristicValueChange((characteristic) => {
+      console.log("接受到按键消息:" + characteristic.characteristicId + ":" + ab2hex(characteristic.value));
       const idx = inArray(this.data.chs, 'uuid', characteristic.characteristicId)
       const data = {}
       if (idx === -1) {
@@ -240,15 +298,55 @@ Page({
       this.setData(data)
     })
   },
-  writeBLECharacteristicValue() {
+  /*写开启报警消息 01 到设备 */
+  writeBLECharacteristicValue01() {
+
+
+    // deviceId: deviceId,
+    //   writeUUid: serviceId,
+    //     writeCharacteristic: res.characteristics[i].uuid
+
+
+    var hexString = "01";
     // 向蓝牙设备发送一个0x00的16进制数据
-    let buffer = new ArrayBuffer(1)
-    let dataView = new DataView(buffer)
-    dataView.setUint8(0, Math.random() * 255 | 0)
+    var typedArray = new Uint8Array(hexString.match(/[\da-f]{2}/gi).map(function (h) {
+      return parseInt(h, 16)
+    }));
+    var buffer = typedArray.buffer;
+    // let dataView = new DataView(buffer)
+    // dataView.setUint8(0, Math.random() * 255 | 0)
+    // dataView.setUint8(0,1);
+    /*使用记录的特征和服务进行发送*/
     wx.writeBLECharacteristicValue({
-      deviceId: this._deviceId,
-      serviceId: this._serviceId,
-      characteristicId: this._characteristicId,
+      deviceId: this.data.deviceId,
+      serviceId: this.data.writeUUid,
+      characteristicId: this.data.writeCharacteristic,
+      value: buffer,
+    })
+  },
+  /*写关闭报警消息 00 到设备 */
+  writeBLECharacteristicValue00() {
+
+
+    // deviceId: deviceId,
+    //   writeUUid: serviceId,
+    //     writeCharacteristic: res.characteristics[i].uuid
+
+
+    var hexString = "00";
+    // 向蓝牙设备发送一个0x00的16进制数据
+    var typedArray = new Uint8Array(hexString.match(/[\da-f]{2}/gi).map(function (h) {
+      return parseInt(h, 16)
+    }));
+    var buffer = typedArray.buffer;
+    // let dataView = new DataView(buffer)
+    // dataView.setUint8(0, Math.random() * 255 | 0)
+    // dataView.setUint8(0, 1);
+    /*使用记录的特征和服务进行发送*/
+    wx.writeBLECharacteristicValue({
+      deviceId: this.data.deviceId,
+      serviceId: this.data.writeUUid,
+      characteristicId: this.data.writeCharacteristic,
       value: buffer,
     })
   },
